3e77d332dc34a89ebe085ac414d8aaec113dc9bd
Gaviria15 <samuelstiven@gmail.com>
2024-03-15 11:03:17 -0500

    Actualización

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 6543;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send("Hola");
+});
+
+app.get('/sesiones', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/sesiones.html'));
+});
+
+app.get('/retos', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/retos.html'));
+});
+
+// Puedes agregar más rutas según sea necesario
+
+app.listen(port, () => {
+    console.log(`La aplicación está escuchando en el puerto ${port}`);
+});
